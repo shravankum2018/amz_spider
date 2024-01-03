3336a95fcef1c18d542f8749fc65484f66ac0d3e
shravankum2018 <shravankum2018@gmail.com>
2024-01-03 11:21:25 +0530

    req1

--- amazon/settings.py
+++ amazon/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = 'amazon.spiders'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
